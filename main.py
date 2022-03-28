6c3df25fb77b29178e3f1396a1af9c966570abd7
CodeByO <code.by.o831@gmail.com>
2022-03-28 20:28:38 +0900

    디렉토리 구조 나눔 및 필요한 파일 생성

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from crypt import methods
 import os
-from dotenv import load_dotenv
 import requests
+
+from dotenv import load_dotenv
+from flask import *
+
 
 load_dotenv(verbose=True)
 
@@ -10,5 +14,58 @@ url = os.getenv('API_URL')
 params = {'serviceKey': api_key, 'LAWD_CD' : '11110','DEAL_YMD':'2021312'}
 
 response = requests.get(url,params=params)
-print(response.content)
 
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/suggestRoom',methods=['POST'])
+def suggestRoom():
+    # if request.method == 'POST':
+    #   id = request.form['id']
+    # reutnr render_template('test.html',form=form)
+    pass
+
+@app.route('/searchRoom',methods=['POST'])
+def suggestRoom():
+    pass
+
+@app.route('/depositQuick',methods=['POST'])
+def depositQuick():
+    pass
+
+@app.route('/depositMerge',methods=['POST'])
+def depositMerge():
+    pass
+
+@app.route('/depositInsert',methods=['POST'])
+def depositInsert():
+    pass
+
+@app.route('/monthlyQuick',methods=['POST'])
+def monthlyQuick():
+    pass
+
+@app.route('/monthlyMerge',methods=['POST'])
+def monthlyMerge():
+    pass
+
+@app.route('/monthlyInsert',methods=['POST'])
+def monthlyInsert():
+    pass
+
+@app.route('/areaQuick',methods=['POST'])
+def monthlyQuick():
+    pass
+
+@app.route('/areaMerge',methods=['POST'])
+def areaMerge():
+    pass
+
+@app.route('/areaInsert',methods=['POST'])
+def areaInsert():
+    pass
---
+++ modules/routes/suggestRoom.py
@@ -0,0 +1,5 @@
+#방 추천 요청 처리
+
+
+class suggestRoom:
+    pass
