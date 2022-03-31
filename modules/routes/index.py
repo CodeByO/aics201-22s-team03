dd88e4a8727cc1fc1e63e504dcff06bcdfa89e8d
CodeByO <code.by.o831@gmail.com>
2022-04-01 00:00:35 +0900

    getData.py 모듈 임포트 및 리턴 작성

--- modules/routes/index.py
+++ modules/routes/index.py
@@ -1,12 +1,21 @@
 from flask import Blueprint, render_template
+import sys,os
 
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from api import getData
 #[Function] 메인페이지
 #[DESC] 클라이언트로 받은 요청 처리
 #[TODO] 검증
 
 blueprint = Blueprint("index", __name__, url_prefix='/')
 
+data = getData.getData()
+
+print (data.getApi())
+
 @blueprint.route('/')
 def index():
+
     return render_template('index.html')
             
