7a77ec1b8703ccd7a4626062d564f0488d76c332
CodeByO <code.by.o831@gmail.com>
2022-04-01 01:22:00 +0900

    필요한 데이터만 파싱해서 리스트화

--- modules/api/getData.py
+++ modules/api/getData.py
@@ -18,13 +18,14 @@ class getData:
         self.url = os.getenv('API_URL')
         self.date = datetime.today().strftime("%Y%m")
     
-    def getApi(self):
-        params = {'serviceKey': self.api_key, 'LAWD_CD' : '36110','DEAL_YMD':self.date}
+    def getApi(self,date):
+        params = {'serviceKey': self.api_key, 'LAWD_CD' : '36110','DEAL_YMD':date}
         response = requests.get(self.url,params=params).content
         
         xmlData = xmltodict.parse(response)
 
         header = xmlData['response']['header']
+        items = xmlData['response']['body']['items']
         resultCode = header['resultCode']
 
         if resultCode != '00':
@@ -33,10 +34,29 @@ class getData:
             print("------------------------------------")
             print(resultMsg)
             print("------------------------------------")
+            return None 
+        elif items == None:
+            print("API 요청 에러 발생")
+            print("------------------------------------")
+            print("해당 하는 데이터가 없음")
+            print("------------------------------------")
             return None
+        item = items['item']
+        print(item[0]["건축년도"])
         
-
-        item = xmlData['response']['body']['items']['item']
-        # data = item['지역코드']['법정동']['계약면적']['월세금액']['보증금액']['건축년도']
+        # test = item['지역코드']['법정동']['계약면적']['월세금액']['보증금액']['건축년도']
+        data = []
+        tmp = []
+        for i in range(len(item)):
+            tmp = []
+            
+            tmp.append(item[i]['지역코드'])
+            tmp.append(item[i]['법정동'])
+            tmp.append(item[i]['계약면적'])
+            tmp.append(item[i]['월세금액'])
+            tmp.append(item[i]['보증금액'])
+            tmp.append(item[i].get('건축년도','0000'))
+            
+            data.append(tmp)
         
-        return item
+        return data
