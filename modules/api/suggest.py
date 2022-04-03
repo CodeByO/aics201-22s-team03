71e0922832c7cccb3bb46cd35aa5c837c2c77543
CodeByO <code.by.o831@gmail.com>
2022-04-03 23:08:41 +0900

    최소 힙 구현 중

--- modules/api/suggest.py
+++ modules/api/suggest.py
@@ -1,3 +1,4 @@
+from asyncio.proactor_events import _ProactorBasePipeTransport
 from calendar import month
 from getData import getData
 
@@ -11,9 +12,45 @@ from getData import getData
 
 
 
-class suggestRoom:
+class suggest:
     def __init__(self):
         data = getData()
         self.monthly, self.charter = data.devideRoom('202203')
-
+        self.N = 0
+        self.input = 0
+        self.endPoint = 0
+        self.heap = [0 for i in range(100001)]
     
+    def suggestMonthly(self):
+        pass
+
+    def suggestCharter(self):
+        pass
+
+    def insertHeap(self, input):
+        self.endPoint += 1
+
+        cur = self.endPoint
+
+        while cur > 0:
+            if self.heap[cur / 2] > input :
+                self.heap.insert(cur,self.heap[cur/2])
+                cur = cur / 2
+            else:
+                break
+        self.heap.insert(cur,input)
+        
+    def popHeap(self):
+        if self.endPoint == 0:
+            print("0")
+            return None
+        root = 1
+        returnValue = self.heap[root]
+        value = self.heap[self.endPoint]
+        self.heap.insert(self.endPoint,0)
+        self.endPoint -= 1
+
+        if self.endPoint == 0:
+            print(returnValue)
+            return None
+        
