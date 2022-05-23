14d37ceffb1f9be74b99b7a0db71bce6c9d97e1b
yoonseo <syuran01@naver.com>
2022-05-23 10:41:31 +0900

    recombine

--- modules/package/sort.py
+++ modules/package/sort.py
@@ -81,8 +81,8 @@ if __name__ == '__main__':
     
     print('----------------quickSort-------------------')
 
-    # for i in roomList:
-    #     print(i[2])
+    for i in roomList:
+        print(i[2])
 
     print('----------------insertSort-------------------')
 
