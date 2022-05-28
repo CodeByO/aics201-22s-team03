4f4c5a4eca7960b06ca9ca3122ab6b57824760cb
CodeByO <code.by.o831@gmail.com>
2022-05-28 17:45:46 +0900

    chechFile 테스트 작성

--- modules/package/tests/testGetData.py
+++ modules/package/tests/testGetData.py
@@ -6,7 +6,7 @@ import csv
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from getData import getData
-
+import asyncio
 #[Function] getData.py 테스트 파일
 #[DESC] getData.py의 각 중요한 메소드 테스트
 #[TODO] test_checkFile 메서드 완성 및 에러 테스트 완성
@@ -22,6 +22,7 @@ from getData import getData
 3. 리스트 안에 데이터가 맞게 들어갔는지
 
 4. 기존에 작성한 에러 처리가 제대로 동작 하는지
+
 '''
 
 
@@ -48,49 +49,32 @@ class getDataTest(unittest.TestCase):
 
     def test_roomList(self):
         for i in self.roomList:
-            self.assertEqual(len(i),6)
+            self.assertEqual(len(i),8)
 
     def test_devideRoom(self):
         for i in self.monthly:
-            self.assertEqual(len(i),6)
+            self.assertEqual(len(i),8)
         for i in self.charter:
-            self.assertEqual(len(i), 6)
+            self.assertEqual(len(i), 8)
     
     def test_getApiError(self):
         pass
 
-
     # def test_checkFile(self):
     #     date = '202204'
-    #     locate = '11000'
-
-    #     count = 0
-    #     count2 = 0
-    #     with open('../roomList.csv','r',encoding='UTF-8') as file:
-    #         checkData = []
-    #         for i in csv.reader(file):
-    #             checkData.append(i)
-    #             count += 1
-    #             if(count == 2):
-    #                 break
-    #     checkDate = "".join(checkData[0])
-    #     checkLocal = "".join(checkData[1])
+    #     locate = '11110'
+    #     roomList = self.data.roomList(date, locate)
+    #     print(roomList[0])
 
 
-    #     self.data.roomList(date,locate)
+    def test_checkFile(self):
+        date = '202104'
+        locate = '11110'
 
-    #     with open('../roomList.csv','r',encoding='UTF-8') as file:
-    #         checkData2 = []
-    #         for i in csv.reader(file):
-    #             checkData2.append(i)
-    #             count2 += 1
-    #             if(count2 == 2):
-    #                 break
-    #     checkDate2 = "".join(checkData[0])
-    #     checkLocal2 = "".join(checkData[1])
+        roomList = self.data.roomList(date,locate)
 
-    #     self.assertNotEqual(checkDate,checkDate2) 
-    #     self.assertNotEqual(checkLocal,checkLocal2)
+        self.assertNotEqual(self.roomList[0],roomList[0]) 
+
 
     # def testDown(self):
     #     try:
